fd70103608ca67f15ae92e94269231d49a11f266
Vikt0r <159viktor159@gmail.com>
2017-02-27 13:00:02 +0300

    added rest-app and web-app

--- model/src/main/java/com/epam/test/dao/User.java
+++ model/src/main/java/com/epam/test/dao/User.java
@@ -1,5 +1,7 @@
 package com.epam.test.dao;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import java.util.Objects;
 
 /**
---
+++ web-app/src/test/java/com/epam/test/web_app/controllers/HelloControllerMockTest.java
@@ -0,0 +1,15 @@
+package com.epam.test.web_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Hello Controller.
+ */
+@Controller
+public class HelloControllerMockTest {
+
+    // TODO: 27.02.17
+}
